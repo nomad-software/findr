54ccf9cd5e6095a2deb506e8589d4bc5c260e61f
Gary Willoughby <dev@nomad.so>
2016-09-16 18:48:03 +0100

    Added default costants to the options.

--- cli/options.go
+++ cli/options.go
@@ -10,6 +10,12 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
+const (
+	DEFAULT_DIRECTORY = "."
+	DEFAULT_REGEX     = ".*"
+	DEFAULT_GLOB      = "*"
+)
+
 type Options struct {
 	Case    bool
 	Dir     string
@@ -23,11 +29,11 @@ func ParseOptions() Options {
 	var opt Options
 
 	flag.BoolVar(&opt.Case, "case", false, "Use to switch to case sensitive pattern matching.")
-	flag.StringVar(&opt.Dir, "dir", ".", "The directory to traverse.")
-	flag.StringVar(&opt.File, "file", "*", "The glob file pattern to match.")
+	flag.StringVar(&opt.Dir, "dir", DEFAULT_DIRECTORY, "The directory to traverse.")
+	flag.StringVar(&opt.File, "file", DEFAULT_GLOB, "The glob file pattern to match.")
 	flag.BoolVar(&opt.Help, "help", false, "Show help.")
 	flag.StringVar(&opt.Ignore, "ignore", "", "A regex to ignore files or directories.")
-	flag.StringVar(&opt.Pattern, "pattern", ".*", "A regex to match files against.")
+	flag.StringVar(&opt.Pattern, "pattern", DEFAULT_REGEX, "A regex to match files against.")
 	flag.Parse()
 
 	opt.Dir, _ = homedir.Expand(opt.Dir)
@@ -56,12 +62,12 @@ func (this *Options) Echo() {
 
 	var output string
 
-	if this.Pattern != ".*" {
+	if this.Pattern != DEFAULT_REGEX {
 		output += color.CyanString("finding:     ")
 		output += color.GreenString("%s\n", this.Pattern)
 	}
 
-	if this.File != "*" {
+	if this.File != DEFAULT_GLOB {
 		output += color.CyanString("files:       ")
 		output += color.GreenString("%s\n", this.File)
 	}
